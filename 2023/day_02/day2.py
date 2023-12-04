1d87b680c50529225d8d087c897eaa01a5805f2f
johanbw <johanbrekstadwijk@gmail.com>
2023-12-04 20:23:00 +0100

    day 2 Part two done

--- 2023/day_02/day2.py
+++ 2023/day_02/day2.py
@@ -3,47 +3,41 @@ f = open("input.txt", "r")
 red_limit = 12
 green_limit = 13
 blue_limit = 14
-ans = 0
+partOne = 0
+partTwo = 0
 
 for line in f:
     gameNr, game = line.split(": ")
     valid_game = True
 
+    min_red = 0
+    min_green = 0
+    min_blue = 0
+
     for set in game.split("; "):
         cubes = set.split(", ")
         for cube in cubes:
             quantity, color = cube.split()
+
             if color == 'red' and int(quantity) > red_limit:
                 valid_game = False
             elif color == 'green' and int(quantity) > green_limit:
                 valid_game = False
             elif color == 'blue' and int(quantity) > blue_limit:
                 valid_game = False
+            
+            if color == 'red':
+                min_red = max(min_red, int(quantity))
+            elif color == 'green':
+                min_green = max(min_green, int(quantity))
+            elif color == 'blue':
+                min_blue = max(min_blue, int(quantity)) 
 
     if valid_game:
         number = int(gameNr.split()[1])
-        ans += number
-print(ans)
+        partOne += number
+    
+    partTwo += min_red * min_green * min_blue
 
-#for line in f:
-#    gameNr, game = line.split(": ")
-#    red_count = 0
-#    green_count = 0
-#    blue_count = 0
-#    for set in game.split("; "):
-#        print(set)
-#        cubes = set.split(", ")
-#        for cube in cubes:
-#            quantity, color = cube.split()
-#            if color == 'red':
-#                red_count += int(quantity)
-#            elif color == 'green':
-#                green_count += int(quantity)
-#            elif color == 'blue':
-#                blue_count += int(quantity)
-#
-#    if red_count <= red_limit and green_count <= green_limit and blue_count <= blue_limit:
-#        print(f"{gameNr} is valid.")
-#        number = int(gameNr.split()[1])
-#        ans += number
-#print(ans)
+print(partOne)
+print(partTwo)
