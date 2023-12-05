b4c47b3b027c4bf94f92268fc5402142a3af517d
johanbw <johanbrekstadwijk@gmail.com>
2023-12-05 19:03:00 +0100

    Heliga Paulson kom till undsättning

--- 2023/day_04/day4.py
+++ 2023/day_04/day4.py
@@ -1,21 +1,33 @@
+from collections import defaultdict
 f = open("input.txt", "r")
 
-def checkwin(correct, hand2check):
+def checkwin(correctCards, myCards):
     wins = 0
-    for i in correct:
-        for hand in hand2check:
-            if i == hand:
+    for correctNum in correctCards:
+        for myNum in myCards:
+            if correctNum == myNum:
                 if wins == 0:
                     wins = 1
                 else:
                     wins *= 2
+
     return wins
 
+D = defaultdict(int)
 ans = 0
-for line in f:
+ 
+for i, line in enumerate(f):
+    D[i] += 1
     game_data = line.strip().split('|')
-
-    winningNums = game_data[0].split(': ')[1].strip().split()
+    winningNums = game_data[0].split(': ')[1].strip().split() 
     myNums = game_data[1].strip().split()
     ans += checkwin(winningNums, myNums)
+
+    #partTwo
+    winsEachCard = len(set(winningNums) & set(myNums))
+    #print(f"val: {winsEachCard}\nd[i]: {i}")
+    for j in range(winsEachCard):
+        D[i+1+j] += D[i]
+
 print(ans)
+print(sum(D.values()))
